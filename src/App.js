9d8e4efdc16254a392ab8c82a1315638fd24d50b
Brian Albert <brian.david.albert@gmail.com>
2023-03-07 12:59:52 -0500

    added Home component with styling

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-
 import './App.css';
+import India from './assets/India.jpg'
+import Logo from './assets/Logo.png'
+import Hospital from './assets/Hospital.jpg'
+import Worker from './assets/Worker.jpg'
+import Hero from './components/Hero';
+
 
 function App() {
   return (
     <div className="App">
-      
+      <Hero imageSrc={India}/>
     </div>
   );
 }
---
+++ src/components/Hero.js
@@ -0,0 +1,13 @@
+import './Hero.css'
+
+
+const Hero = ({imageSrc}) => {
+  return (
+    <div className="hero">
+        <img src={imageSrc} alt="EH India Photo" className="hero-image" />
+        <h1 className="hero-title">Amplify Her Voice</h1>
+    </div>
+  )
+}
+
+export default Hero
