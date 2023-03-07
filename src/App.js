5ce5a0f95e9dc5ac74dbac4ee51bc60bf1c46acb
Brian Albert <brian.david.albert@gmail.com>
2023-03-07 13:49:20 -0500

    added slider component with styling

--- src/App.js
+++ src/App.js
@@ -4,12 +4,23 @@ import Logo from './assets/Logo.png'
 import Hospital from './assets/Hospital.jpg'
 import Worker from './assets/Worker.jpg'
 import Hero from './components/Hero';
+import Slider from './components/Slider';
 
 
 function App() {
   return (
     <div className="App">
       <Hero imageSrc={India}/>
+      <Slider imageSrc={Worker} 
+              title={`It's time to amplify her voice!`} 
+              subtitle={'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Libero, dicta.'}
+              flipped={false}
+              />
+      <Slider imageSrc={Hospital} 
+              title={'Stand up for SRHR!'} 
+              subtitle={'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Libero, dicta.'}
+              flipped={true}                
+              />
     </div>
   );
 }
