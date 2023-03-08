1c04d64070f0ee44307e69b16c33abb2ba3ed817
Brian Albert <brian.david.albert@gmail.com>
2023-03-08 07:48:33 -0500

    created build folder and firebase config files for deployment

---
+++ src/firebase/config.js
@@ -0,0 +1,13 @@
+import firebase from 'firebase/app'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCXM0HbxuvIT0AiNOT6ZsgyQTAnzgM1nNE",
+    authDomain: "amplify-her-voice.firebaseapp.com",
+    projectId: "amplify-her-voice",
+    storageBucket: "amplify-her-voice.appspot.com",
+    messagingSenderId: "784608601045",
+    appId: "1:784608601045:web:9662eb5dfc1435391e135c",
+    measurementId: "G-XN9ZP8KPK6"
+  }
+
+  firebase.initializeApp(firebaseConfig)
